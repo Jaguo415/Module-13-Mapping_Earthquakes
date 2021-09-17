434f0cd4f7450a6569f0e30ee71b365d056535ad
Jaguo415 <83923903+Jaguo415@users.noreply.github.com>
2021-09-17 11:30:46 -0700

    13.4.1

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -1,55 +1,20 @@
 console.log("working");
 
 // We create the map object with options.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
-let cities = [{
-	location: [40.7128, -74.0059],
-	city: "New York City",
-	state: "NY",
-	population: 8398748
-  },
-  {
-	location: [41.8781, -87.6298],
-	city: "Chicago",
-	state: "IL",
-	population: 2705994
-  },
-  {
-	location: [29.7604, -95.3698],
-	city: "Houston",
-	state: "TX",
-	population: 2325502
-  },
-  {
-	location: [34.0522, -118.2437],
-	city: "Los Angeles",
-	state: "CA",
-	population: 3990456
-  },
-  {
-	location: [33.4484, -112.0740],
-	city: "Phoenix",
-	state: "AZ",
-	population: 1660272
-  }
-  ];
+//  Add a circle to the map.
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+// L.circle([34.0522, -118.2437], {
+// 	radius: 100
+// }).addTo(map);
 
-// Get data from cities.js
-let cityData = cities;
-
-// Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-	console.log(city)
-	L.circleMarker(city.location, {
-		radius: city.population/100000,
-		color: "orange",
-		weight: 4
-	})
-	.bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-  .addTo(map);
-});
-
+//  Add a circle to the map
+L.circleMarker([34.0522, -118.2437], {
+	radius: 300,
+	color: "black",
+	fillColor: '#ffffa1'
+}).addTo(map);
 
 
 // We create the tile layer that will be the background of our map.
@@ -60,4 +25,3 @@ let darkmap = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tile
 });
 
 // Then we add our 'graymap' tile layer to the map.
-darkmap.addTo(map);
